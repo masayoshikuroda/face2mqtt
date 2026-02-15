f4c2996c9e81000c65adb489e9b127f8aac962cc
masayoshikuroda <kuroda.masayoshi@gmail.com>
2026-02-15 21:37:05 +0900

    Enhance IPCamera initialization to support rotation and flipping options

--- ipcamera.py
+++ ipcamera.py
@@ -3,13 +3,30 @@ import cv2
 import numpy as np
 
 class IPCamera:
-    def __init__(self, url):
+    def __init__(self, url, rotate=0, flip=0):
         self.url = url
+        self.rotate = rotate
+        self.flip = flip
 
     def take_snapshot(self):
         response = requests.get(self.url)
         image_array = np.frombuffer(response.content, dtype=np.uint8)
         img_bgr = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
         img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+        
+        if self.rotate == 1:
+            img_rgb = cv2.rotate(img_rgb, cv2.ROTATE_90_CLOCKWISE)
+        elif self.rotate == 2:
+            img_rgb = cv2.rotate(img_rgb, cv2.ROTATE_180_CLOCKWISE)
+        elif self.rotate == 3:
+            img_rgb = cv2.rotate(img_rgb, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        
+        if self.flip == 1:
+            img_rgb = cv2.flip(img_rgb, 0)
+        elif self.flip == 2:
+            img_rgb = cv2.flip(img_rgb, 1)
+        elif self.flip == 3:
+            img_rgb = cv2.flip(img_rgb, -1)
+
         return img_rgb
 
--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from paho.mqtt import client as mqtt_client
 from ipcamera import IPCamera
 from facedetector import FaceDetector
 
-MQTT_BROKER     = os.getenv('MQTT_BROKER',   'mqtt.local')
-MQTT_PORT       = os.getenv('MQTT_PORT',     '1883')
-MQTT_TOPIC      = os.getenv('MQTT_TOPIC',    'face2mqtt')
-MQTT_DATA       = os.getenv('MQTT_DATA',     '{\"type\":\"start\"}')
-IP_CAMERA_URL   = os.getenv('IP_CAMERA_URL', 'http://localhost:8081')
-DETECT_LEVEL    = os.getenv('DETECT_LEVEL',  '0.7')
-DETECT_INTERVAL = os.getenv('DETECT_LEVEL',  '1.0')
+MQTT_BROKER      = os.getenv('MQTT_BROKER',      'mqtt.local')
+MQTT_PORT        = os.getenv('MQTT_PORT',        '1883')
+MQTT_TOPIC       = os.getenv('MQTT_TOPIC',       'face2mqtt')
+MQTT_DATA        = os.getenv('MQTT_DATA',        '{\"type\":\"start\"}')
+IP_CAMERA_URL    = os.getenv('IP_CAMERA_URL',    'http://localhost:8081')
+IP_CAMERA_ROTATE = os.getenv('IP_CAMERA_ROTATE', '0')
+IP_CAMERA_FLIP   = os.getenv('IP_CAMERA_FLIP',   '0')
+DETECT_LEVEL     = os.getenv('DETECT_LEVEL',     '0.7')
+DETECT_INTERVAL  = os.getenv('DETECT_LEVEL',     '1.0')
 
 def on_connect(client, userdata, flags, reason_code, properties):
     if reason_code == 0:
@@ -34,7 +36,7 @@ client.on_disconnect = on_disconnect
 client.connect(MQTT_BROKER, int(MQTT_PORT))
 client.loop_start()
 
-camera = IPCamera(IP_CAMERA_URL)
+camera = IPCamera(IP_CAMERA_URL, IP_CAMERA_ROTATE, IP_CAMERA_FLIP)
 detector = FaceDetector()
 
 while(True):
